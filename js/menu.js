3109b6a202ecb541992a54175fe47ae0ca2947d2
rauleibanez <83990877+rauleibanez@users.noreply.github.com>
2022-10-27 23:23:13 -0500

    Seccion de perfil y seccion de Habilidades

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,13 @@
+/*--------------------------------------------------------
+ * Archivo    : menu.js;
+ * Descripcion: Archivo de JavaScript para el dinamismo de
+                la pagina (index.html);
+ * Autor      : Raul Ibañez;
+ * Version    : 1.0;
+ * Fecha      : 26/10/2022;
+ * Observacion: Diseño Mobile First;
+ */
+/*---------- FUNCION PARA ACTIVAR MENU DISPOSITIVO MOVIL----*/
 const menu = document.querySelector(".menu");
 const openMenuBtn = document.querySelector(".open-menu");
 const closeMenuBtn = document.querySelector(".close-menu");
@@ -8,3 +18,4 @@ function toggleMenu(){
 
 openMenuBtn.addEventListener("click", toggleMenu);
 closeMenuBtn.addEventListener("click", toggleMenu);
+/*------------------------------------------------------------*/
